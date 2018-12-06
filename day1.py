8490a4016c9c122ac234a3fa02b8260538b6d5ce
Janessa Tran <tranjanessa@gmail.com>
2018-12-05 21:15:39 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -34,3 +34,40 @@ data = pd.read_csv('data/day1_freqs.txt', sep=" ", header=None)
 data.columns = ["frequency"]
 answer = sum(data['frequency'])
 print(answer)
+
+"""
+
+Your puzzle answer was 510.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+You notice that the device repeats the same frequency change list over and over. To calibrate the device, you need to find the first frequency it reaches twice.
+
+For example, using the same list of changes above, the device would loop as follows:
+
+Current frequency  0, change of +1; resulting frequency  1.
+Current frequency  1, change of -2; resulting frequency -1.
+Current frequency -1, change of +3; resulting frequency  2.
+Current frequency  2, change of +1; resulting frequency  3.
+(At this point, the device continues from the start of the list.)
+Current frequency  3, change of +1; resulting frequency  4.
+Current frequency  4, change of -2; resulting frequency  2, which has already been seen.
+In this example, the first frequency reached twice is 2. Note that your device might need to repeat its list of frequency changes many times before a duplicate frequency is found, and that duplicates might be found while in the middle of processing the list.
+""
+
+
+def first_duplicate(vals):
+    seen = set()
+    dup_flag = False
+    curr_sum = 0
+    while(dup_flag == False):
+        for v in vals: 
+            curr_sum += v
+            if curr_sum in seen and len(seen)>1:
+                dup_flag = True  
+                return curr_sum
+            seen.add(curr_sum) 
+
+answer = first_duplicate(data["frequency"].tolist())
+print(answer)
